8fe468cdfc849b26b14009a3ef3cfaccd02754cf
Luca Graef <lumagr.mail@gmail.com>
2024-05-24 10:36:19 +0200

    Bugfixes

--- runner.py
+++ runner.py
@@ -42,7 +42,7 @@ def generate_inhaltsverzeichnis(data):
 
     html += """
     </ul>
-    <p class="right">Heiner Giefers, 05-2024</p>
+    <p class="right">""" + footer + """</p>
     <div class='page-break'></div>
     """
     return html
@@ -70,18 +70,28 @@ def create_pdf(filtered_content):
     <body>
     """
 
+    # Durchlaufe alle gefundenen Lösungen
     for file in filtered_content:
         for cell in file:
+            # Dateinamen als Überschrift hinzufügen
             html_content += f"<h1>{remove_prefix(cell)}</h1><hr/>"
             for number in file[cell]:
+                # Füge die Lösung zur HTML-Seite hinzu, entferne dabei die Lösungsmarkierungen
                 for row in number['source']:
                     if "### BEGIN SOLUTION" not in row and "### END SOLUTION" not in row:
                         html_content += f"<pre>{row}</pre>"
+                # Füge eine Trennlinie zwischen den Lösungen hinzu
                 html_content += "<br/><hr/><br/>"
+            # Füge eine Fußleiste hinzu
             html_content += "<p class='right'>" + footer +"</p><div class='page-break'></div>"
 
+    # Schließe den HTML-Body
     html_content += "</body>"
+
+    # Generiere das Inhaltsverzeichnis und füge es vor den Inhalt ein
     complete_html = generate_inhaltsverzeichnis(filtered_content) + html_content
+
+    # Erstelle das PDF-Dokument
     pdfkit.from_string(complete_html, "target/solutions.pdf")
 
 
@@ -97,12 +107,16 @@ def process_notebook_files_in_directory(directory):
         list: Eine Liste mit gefundenen Lösungen.
     """
     solutions = []
+    # Durchsuche alle Ordner im Verzeichnis
     for root, _, files in os.walk(directory):
+        # Durchlaufe gefundene Datein in Ordner
         for file_name in files:
+            # Prüfe, ob Datei eine .ipynb Datei ist
             if file_name.endswith('.ipynb'):
                 file_path = os.path.join(root, file_name)
                 try:
                     found_solutions = []
+                    # Öffne die Datei und lade den Inhalt
                     with open(file_path, "r", encoding='utf-8') as file:
                         notebook_content = json.load(file)
                         if "cells" in notebook_content:
@@ -110,7 +124,9 @@ def process_notebook_files_in_directory(directory):
                                 if "source" in cell:
                                     source = "".join(cell["source"])
                                     if "### BEGIN SOLUTION" in source and "### END SOLUTION" in source:
+                                        # Füge die Zelle zur Liste der gefundenen Lösungen für dieses Notebook hinzu
                                         found_solutions.append(cell)
+                    # Füge die gefundenen Lösungen für dieses Notebook zur Gesamtliste hinzu
                     solutions.append({file_name.replace(".ipynb", ""): found_solutions})
                 except Exception as e:
                     print(f"Konnte Datei {file_path} nicht öffnen: {e}")
@@ -134,7 +150,7 @@ def read_paths_from_file(file_path):
 
 def main():
     """
-    Hauptfunktion des Programms. Liest die Pfade ein, verarbeitet die Notizbuchdateien
+    Liest die Pfade ein, verarbeitet die Notizbuchdateien
     und erstellt ein PDF-Dokument mit den gefundenen Lösungen.
     """
     paths_file = 'files.txt'
